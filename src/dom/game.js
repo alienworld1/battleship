15a11c12e94fc5ba36bf58fb799f2b837e9ab997
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-03-17 22:13:17 +0530

    Render both human and computer boards

--- src/dom/game.js
+++ src/dom/game.js
@@ -1,6 +1,26 @@
 import './styles/game.css';
+import Board from './board';
 
 const Game = document.createElement('main');
 Game.id = 'game-screen';
+
+const BoardComponent = (board, label) => {
+    const boardComponent = document.createElement('div');
+    boardComponent.classList.add('board-component');
+
+    boardComponent.appendChild(board);
+
+    const labelElement = document.createElement('h3');
+    labelElement.textContent = label;
+    boardComponent.appendChild(labelElement);
+
+    return boardComponent;
+};
+
+const HumanBoard = new Board('human-board');
+const ComputerBoard = new Board('computer-board');
+
+Game.appendChild(BoardComponent(HumanBoard.board, 'You'));
+Game.appendChild(BoardComponent(ComputerBoard.board, 'Computer'));
 
 export default Game;
